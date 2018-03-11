b264cbfbc69c3b04ae5f52e80254802af9fc860e
lkush <l.kushnariova@yandex.ru>
2018-03-11 23:09:42 +0300

    Task implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let zeroes = 0
+    let primeBase = getMaxPrime(base)
+    while (number >= primeBase) {
+        number = Math.floor(number / primeBase)
+        zeroes += number
+    }
+
+    let primeBaseCount = 0
+    while (base % primeBase === 0) {
+        base = base / primeBase
+        primeBaseCount++
+    }
+
+    return Math.floor(zeroes / primeBaseCount)
+}
+
+function getMaxPrime(number) {
+    let maxPrime
+
+    nextPrime:
+        for (let i = 2; i <= number; i++) {
+            for (let j = 2; j < i; j++) {
+                if (i % j === 0) continue nextPrime
+            }
+            if (number % i === 0) maxPrime = i
+        }
+
+    return maxPrime
 }
